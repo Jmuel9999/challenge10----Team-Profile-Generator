906a818a37f116f012046101292639ebf32808ea
jasonmueller <jmuel9999@gmail.com>
2021-05-28 17:49:29 -0500

    Started questionPrompt with inquirer on index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // include packages needed for this application
 const inquirer = require('inquirer');
 
+// begining user prompts for team information
+const questionPrompt = () => {
+    return inquirer.prompt([
+        type: 'input',
+        name: 'manager',
+        message: 'What is the name of the team manager?'
+    ]);
+};
