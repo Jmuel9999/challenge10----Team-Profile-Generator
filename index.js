9eebea94567dd89798209e10a4b1324c0cdd558f
jasonmueller <jmuel9999@gmail.com>
2021-05-28 19:41:53 -0500

    generatePage function created and tested with Team Manager information OK

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // include packages needed for this application
 const inquirer = require('inquirer');
+// object destructuring call
+const {generatePage} = require('./lib/generatePage');
+const fs = require('fs');
 
 // begining user prompts for team information
 const questionPrompt = () => {
@@ -57,13 +60,42 @@ const questionPrompt = () => {
                     return false;
                 }
             }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the team office number?", 
+            validate: officenumber => {
+                if (officenumber) {
+                    return true;
+                } else {
+                    console.log('Please enter team office number!');
+                    return false;
+                }
+            }
         }
     ]);
 };
 
 // create a function to write HTML. writeToFile
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        if (err) return console.log(err);
+        console.log(data)
+    })
+};
 
-questionPrompt();
-    // .then(data => {
-    //     writeToFile('homepage.html', generateHTML(data));
-    // })
+
+
+// // TODO: Create a function to initialize app
+function init() {
+    questionPrompt()
+    .then(data => {
+        // Creates html page and takes the json object that we made (inquirer.prompt) and turns it into a string
+        writeToFile('index.html', generatePage(data));
+        //console.log(data);
+    }) 
+};
+
+// Function call to initialize app
+init();
